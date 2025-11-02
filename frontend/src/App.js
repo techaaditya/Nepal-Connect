e981f82d5329d5698202011371609e30e24db0a8
Aaditya Sapkota <sayhi.aaditya@gmail.com>
2025-11-02 22:32:15 +0545

    Set Router basename to '/Nepal-Connect'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ import { AuthProvider } from './AuthContext';
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Router basename="/Nepal-Connect">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<HomePage />} />
